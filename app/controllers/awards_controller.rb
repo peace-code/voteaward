dbbcd3db2d244d7f9aec28dc9add6c99953a9bc0
KWEON Ohyeon <rest515@gmail.com>
2012-12-08 23:02:00 +0900

    Award : share after creating award

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -6,6 +6,10 @@ class AwardsController < ApplicationController
   def create
     @award = current_user.awards.build(params[:award])
     if @award.save
+      current_user.facebook.put_wall_post(
+        I18n.t('award.share', username: current_user.name, title: @award.title, content: @award.content, prize: @award.prize),
+        { name: I18n.t('g.title'), link: awards_url }
+      )
       redirect_to promises_path, notice: I18n.t('award.created')
     else
       render action: 'new'
