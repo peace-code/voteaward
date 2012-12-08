7b05c3349e853a2d031adc3bf61dc6d3fe018d39
KWEON Ohyeon <rest515@gmail.com>
2012-12-08 17:21:06 +0900

    separate Sequence

--- app/models/promise.rb
+++ app/models/promise.rb
@@ -28,14 +28,3 @@ class Promise
     self.seq = Sequence.generate_id(:promise)
   end
 end
-
-class Sequence
-  include Mongoid::Document
-  field :object
-  field :last_id, type: Integer
-
-  def self.generate_id(object)
-    @seq=where(:object => object).first || create(:object => object)
-    @seq.inc(:last_id,1)
-  end
-end
---
+++ app/models/sequence.rb
@@ -0,0 +1,15 @@
+class Sequence
+  include Mongoid::Document
+  field :object
+  field :last_id, type: Integer
+
+  def self.generate_id(object)
+    @seq=where(:object => object).first || create(:object => object)
+    @seq.inc(:last_id,1)
+  end
+
+  def self.get_last_id(object)
+    @seq=where(:object => object).first
+    (@seq.blank?) ? 0 : @seq.last_id
+  end
+end
