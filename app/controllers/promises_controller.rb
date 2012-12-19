660098dc0beeaec87c1eb2f37ec8a329f348ccd9
KWEON Ohyeon <rest515@gmail.com>
2012-12-19 10:20:29 +0900

    change encoure message & finish promise creation

--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -9,17 +9,18 @@ class PromisesController < ApplicationController
 
   def create
     @promise = current_user.build_promise(params[:promise])
-    if @promise.save
-      message = I18n.t('promise.share', username: current_user.name, reason: @promise.reason, seq: @promise.seq)
-      if current_user.omniauth_provider == :twitter
-        current_user.twitter.update([promises_url, message, I18n.t('g.hashtag')].join(' '))
-      else
-        current_user.facebook.put_wall_post(message, {name: I18n.t('g.title'), link: promises_url})
-      end
-      redirect_to promises_path, notice: I18n.t('promise.created')
-    else
-      render action: 'new'
-    end
+    redirect_to promises_url
+    # if @promise.save
+    #   message = I18n.t('promise.share', username: current_user.name, reason: @promise.reason, seq: @promise.seq)
+    #   if current_user.omniauth_provider == :twitter
+    #     current_user.twitter.update([promises_url, message, I18n.t('g.hashtag')].join(' '))
+    #   else
+    #     current_user.facebook.put_wall_post(message, {name: I18n.t('g.title'), link: promises_url})
+    #   end
+    #   redirect_to promises_path, notice: I18n.t('promise.created')
+    # else
+    #   render action: 'new'
+    # end
   end
 
   def show
