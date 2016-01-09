5ac7dba7111b88249f3ef286994de2360b1a071a
wagurano <wagurano@gmail.com>
2016-01-10 02:24:33 +0900

    update image_uploader
    change from rmagick to mini_magic
    change from fog-awe to fog-local

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,14 +2,16 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
-  include CarrierWave::RMagick
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  include Sprockets::Helpers::RailsHelper
-  include Sprockets::Helpers::IsolatedHelper
+  # include Sprockets::Helpers::RailsHelper # 4.0
+  # include Sprockets::Helpers::IsolatedHelper # 4.0
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  # storage :fog
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
