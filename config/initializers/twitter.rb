5538a706bd243caf3de838fb2ccd09f4b037ad66
wagurano <wagurano@gmail.com>
2016-01-10 02:27:08 +0900

    update twitter
    twitter gem do not use global constant for thread-safe

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-Twitter.configure do |config|
-  config.consumer_key = Rails.configuration.twitter['client_id']
-  config.consumer_secret = Rails.configuration.twitter['client_secret']
-end
+# Twitter.configure do |config|
+#   config.consumer_key = Rails.configuration.twitter['client_id']
+#   config.consumer_secret = Rails.configuration.twitter['client_secret']
+# end
